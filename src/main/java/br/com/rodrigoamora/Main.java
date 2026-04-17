4e793031dbb92888809b6eab04b515490eddcac1
RodrigoAmora <rodrigo.amora.freitas@gmail.com>
2026-04-17 17:50:44 -0300

    Initial commit

---
+++ src/main/java/br/com/rodrigoamora/Main.java
@@ -0,0 +1,17 @@
+package br.com.rodrigoamora;
+
+import br.com.rodrigoamora.vitrual_htreads.VirtualThreads;
+
+import java.util.concurrent.ExecutionException;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+
+        VirtualThreads.startVirtualThreads();
+    }
+}
